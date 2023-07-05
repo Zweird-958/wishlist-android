67bebab32a79f8d6964e1122b4ca927ea7e66d14
HAMEL Julien <julienhamel.h@gmail.com>
2023-07-05 23:26:50 +0200

    feat: display image of wish

--- app/src/main/java/com/example/wishlist_android/pages/Wishlist.kt
+++ app/src/main/java/com/example/wishlist_android/pages/Wishlist.kt
@@ -1,17 +1,22 @@
 package com.example.wishlist_android.pages
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.example.wishlist_android.wishlist
 
 @Composable
@@ -29,10 +34,27 @@ fun Wishlist(navController: NavController) {
         LazyColumn {
             wishlist.forEach { wish ->
                 item {
-                    Card(modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)) {
-                        Text(text = wish.name)
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 8.dp),
+                    ) {
+                        Row(modifier = Modifier.padding(16.dp)) {
+
+                            AsyncImage(
+
+                                model = wish.image,
+                                contentDescription = null,
+                                modifier = Modifier.size(height = 160.dp, width = 130.dp)
+                                    .background(color = Color.Red),
+                            )
+                            Column {
+                                Text(text = wish.name)
+                                Text(text = wish.price.toString())
+
+                            }
+
+                        }
                     }
                 }
             }
